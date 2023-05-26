aafb608ecc5a653dec5928d27cc28fc02f52262a
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-05-26 15:38:25 +0300

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,14 @@
-class solution(object):
-    def fizzbuzz(self,n):
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
         list =[]
         for i in range(1,n+1):
-            if i%3 == 0:
-                list.append("Fizz")
+            if i%3 == 0:  
                 if i% 5 ==0:
                     list.append("FizzBuzz")
+                    continue
+                list.append("Fizz")
             elif i%5 == 0:
                 list.append("Buzz")
             else:
-                list.append(i)
+                list.append(str(i))
         return list
