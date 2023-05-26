378299a0f14b296138c45a28889d718df2ec4e2d
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-05-26 15:31:13 +0300

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,13 @@
-user = int(input("n: "))
-list =[]
-for i in range(1,int(user)+1):
-    if i%3 == 0:
-        list.append("Fizz")
-        if i% 5 ==0:
-            list.append("FizzBuzz")
-    elif i%5 == 0:
-        list.append("Buzz")
-    else:
-        list.append(i)
-print(list)
+class solution(object):
+    def fizzbuzz(self,n):
+        list =[]
+        for i in range(1,n+1):
+            if i%3 == 0:
+                list.append("Fizz")
+                if i% 5 ==0:
+                    list.append("FizzBuzz")
+            elif i%5 == 0:
+                list.append("Buzz")
+            else:
+                list.append(i)
+        return list
