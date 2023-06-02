f7f9e863e25ae59acd976107566ced59e3382da4
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-06-02 10:23:46 +0300

    Update CountingSort1.py

--- CountingSort1.py
+++ CountingSort1.py
@@ -1,8 +1,6 @@
 def countingSort1(arr):
     # Write your code here
-    result =[]
-    for i in range(100):
-        result.append(0) 
+    result =[0]*100
     for i in range(len(arr)):
         result[arr[i]] = result[arr[i]] + 1
     return result
