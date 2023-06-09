21f867affc38dedf4d8c037d94e2173e2974396e
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-06-09 12:45:31 +0300

    Update Binaryinterpreter.py

--- Binaryinterpreter.py
+++ Binaryinterpreter.py
@@ -1,13 +1,18 @@
 def binarytonumber(binary):
     binary = binary[::-1]
     num = 0
-
     for i in range(len(binary)):
         if binary[i] == "1":
-            num += 2**i
-        else:
-            num+=0     
+            num += 2**i     
     return num
+def numbertobinary(number):
+    binary =''
+    while number>0:
+        if number %2 == 0:
+            binary += "0"
+        elif number%2 != 0:
+            binary += "1"
+        number = number // 2
+    return binary[::-1]
 
 
-print(binarytonumber("1010101010111"))
