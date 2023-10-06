6ab2445f0f4b9dc8a4c9df6a3cb6690b15f39153
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-10-06 22:34:32 +0300

    Add files via upload

---
+++ Boats_to_Save_People.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        people.sort()
+        start = 0
+        end = len(people)-1
+        nboats = 0
+        while start <=end:
+            if (people[start] + people[end]) <= limit:
+                start+=1
+                end-=1
+            else:
+                end-=1
+            nboats+=1
+        return nboats          
