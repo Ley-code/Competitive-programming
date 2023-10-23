739a8d7099718d1a5f1092fbc293817dbea94dfc
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-10-23 20:47:08 +0300

    Add files via upload

---
+++ Max_Consecutive_Ones.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums):
+        count = maxcount = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                count+=1
+                if count>maxcount:
+                    maxcount = count
+            else:
+                count=0
+        return maxcount
+    
