d078dd34bfcb66a4a47723ef90997efac99ea230
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-10-25 20:00:05 +0300

    Update Two_sums.py

--- Two_sums.py
+++ Two_sums.py
@@ -1,6 +1,9 @@
-def twosums(nums,target):
-    for i in range(len(nums)-1):
-        for j in range(i+1,len(nums)):
-            if nums[i]+nums[j]==target:
-                return [i,j]
-print(twosums([3,2,4],6))
+class Solution(object):
+    def twoSum(self, nums, target):
+        nummap = {}
+        for i in range(len(nums)):
+            value = target - nums[i]
+            if value in nummap:
+                return [nummap[value],i]
+            nummap[nums[i]] = i
+        return []
