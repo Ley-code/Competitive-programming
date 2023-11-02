fcd64a7bf7579f7215bb639fb4a794028cbeac9b
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-11-02 22:15:26 +0300

    Add files via upload

---
+++ Replace_Elements_in_an_Array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def arrayChange(self, nums, operations):
+        hashmap = {}
+        for i,x in enumerate(nums):
+            hashmap[x] = i
+        for x,y in operations:
+            print(x,y)
+            index = hashmap[x]
+            nums[index] = y
+            hashmap[y] = index
+            del hashmap[x]
+        return nums
