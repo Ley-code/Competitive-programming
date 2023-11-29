fc4ab5ed0ddf915481d7afcf60782741b6b9aa32
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-11-29 18:51:18 +0300

    Added solution for Palindrome Number

---
+++ onboarding/week1/palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        reversed = 0
+        num = x
+        while num!= 0:
+            digit = num%10
+            reversed = reversed*10 + digit
+            num//=10
+        return reversed == x
