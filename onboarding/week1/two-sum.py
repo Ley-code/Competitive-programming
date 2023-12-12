634c8da6cc0f5451359eeba750aef1c5b52c7599
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-12-12 18:11:18 +0300

    Added solution for Two Sum

---
+++ onboarding/week1/two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [hashmap[complement],i]
+            hashmap[nums[i]] = i
+        
