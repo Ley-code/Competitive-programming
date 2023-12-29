9c6d0cbd708386ec117872ba92a3327724c4be2e
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-12-29 18:02:50 +0300

    Added solution for Largest Number

---
+++ EducationPhase/largest-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for _ in range(len(nums)-1):
+            for i in range(len(nums)-1):
+                x = str(nums[i])
+                y = str(nums[i+1])
+                if x+y < y+x:
+                    nums[i],nums[i+1] = nums[i+1],nums[i]
+        
+        return str(int("".join(list(map(str,nums))))) 
