5cd574a7a673615db567309e7bc3b3ac5b3c6fb8
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-01-03 19:15:05 +0300

    Added solution for String Compression

--- EducationPhase/string-compression.py
+++ EducationPhase/string-compression.py
@@ -1,24 +1,26 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        l = r = i = 0
-        while r<len(chars):
-            if chars[r]!=chars[l]:
+        l = 0
+        i = 0
+        count = 0
+        for r in range(len(chars)):
+            if chars[l]==chars[r]:
+                count+=1
+            else:
                 chars[i] = chars[l]
-                i+=1   
-                if r-l > 1:
-                    length = str(r-l)
+                i+=1
+                if count>1:
+                    length = str(count)
                     for j in range(len(length)):
                         chars[i] = length[j]
                         i+=1
-                l = r
-            r+=1
-            
+                count = 1
+                l=r
         chars[i] = chars[l]
         i+=1
-        if r-l > 1:
-            length = str(r-l)
-            for k in range(len(length)):
-                chars[i] = length[k]
+        if count>1:
+            length = str(count)
+            for j in range(len(length)):
+                chars[i] = length[j]
                 i+=1
         return i
-            
