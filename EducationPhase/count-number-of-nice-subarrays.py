20ec4838f5e6af193ad448799d4bb2389bc9fa34
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-01-11 18:19:23 +0300

    Added solution for Count Number of Nice Subarrays

---
+++ EducationPhase/count-number-of-nice-subarrays.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numberOfSubarrays(self, nums, k):
+        ans = left = cnt = odd = 0
+        for i in range(len(nums)):
+            if nums[i]%2!=0:
+                odd+=1
+                cnt = 0
+            while odd == k:
+                cnt+=1
+                odd-=nums[left]&1
+                left+=1
+            ans+=cnt
+        return ans
