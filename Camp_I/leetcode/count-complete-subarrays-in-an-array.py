dd462b2b99239238708c7b193954830f1f93cd80
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-02-09 09:53:20 +0300

    Add solution for Count Complete Subarrays in an Array

--- Camp_I/leetcode/count-complete-subarrays-in-an-array.py
+++ Camp_I/leetcode/count-complete-subarrays-in-an-array.py
@@ -1,16 +1,16 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        distinctele = len(Counter(nums))
+        distinctele = len(set(nums))
         l = 0
         count = 0
         countmap = defaultdict(int)
         for r in range(len(nums)):
-            for j in range(r,len(nums)):
-                countmap[nums[j]]+=1
-                if len(countmap)==distinctele:
-                    count+=1
-            countmap = defaultdict(int)
-            if countmap[nums[r]]==0:
-                del countmap[nums[r]]
+            countmap[nums[r]]+=1
+            while len(countmap)==distinctele:
+                countmap[nums[l]]-=1
+                count+=len(nums)-r
+                if countmap[nums[l]]==0:
+                    del countmap[nums[l]]
+                l+=1       
         return count
 
