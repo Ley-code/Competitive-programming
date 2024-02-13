4682cfa6424b4bf917474b8a34a7663b3c5a9577
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-02-13 06:37:25 +0300

    Add solution for Find First Palindromic String in the Array

---
+++ Camp_I/leetcode/find-first-palindromic-string-in-the-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        def checkpal(word):
+            l = 0
+            r = len(word)-1
+            while l<= r:
+                if word[l]!=word[r]:
+                    return False
+                l+=1
+                r-=1
+            return True
+        for word in words:
+            if checkpal(word):
+                return word
+        return ""
