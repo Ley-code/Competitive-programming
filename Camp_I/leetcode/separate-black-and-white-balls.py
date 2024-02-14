21ae227c79fb0b199e84d5a653fef8ce96baa5f2
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-02-14 05:19:02 +0300

    Add solution for Separate Black and White Balls

---
+++ Camp_I/leetcode/separate-black-and-white-balls.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        count = 0
+        ans = 0
+        for i in range(len(s)):
+            if s[i] == "1":
+                count+=1
+            else:
+                ans+=count
+        return ans
+            
+
+            
