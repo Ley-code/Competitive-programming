e8df7d5064b025b9613a4107d7126e8ca67449c6
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-02-25 21:19:07 +0300

    Add solution for Jump Game

--- Camp_I/leetcode/jump-game.py
+++ Camp_I/leetcode/jump-game.py
@@ -1,12 +1,13 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        curr = len(nums)-1
-        for i in range(len(nums)-2,-1,-1):
-            if i+nums[i]>=curr:
-                curr = i
-        if curr==0:
-            return True
-        return False
+        curr = nums[0]
+        for i in range(1,len(nums)):
+            if curr == 0:
+                return False
+            curr-=1
+            curr = max(curr,nums[i])
+        return True
+            
             
             
             
