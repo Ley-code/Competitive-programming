aba9ad24bc45ec6f7dfea6a0e32c3e8c52ebeb24
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-02-26 10:44:09 +0300

    Add solution for Combinations

---
+++ Camp_I/leetcode/combinations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = []
+        def backtrack(nums,combination):
+            if len(combination)==k:
+                combination.sort()
+                ans.append(combination)
+                return
+            if nums>n:
+                return
+            backtrack(nums+1,combination+[nums])
+            backtrack(nums+1,combination)
+            
+
+        backtrack(1,[])
+        return ans
