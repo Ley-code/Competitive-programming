c1619b1a03086424de1d8bff996fecfedd48ac43
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-03-01 09:40:43 +0300

    Add solution for Dota2 Senate

--- Camp_I/leetcode/dota2-senate.py
+++ Camp_I/leetcode/dota2-senate.py
@@ -1,26 +1,23 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        nradiant = senate.count("R")
-        ndire = senate.count("D")
-        haveR = 0
-        haveD = 0
-        poppedR = 0
-        poppedD = 0
-        queue = deque(list(senate))
-        l = 0
-        while poppedR != nradiant and poppedD!=ndire:
-            if queue[l] == "R" and haveD:
-                poppedR+=1
-                haveD-=1
-            elif queue[l] == "R" and not haveD:
-                haveR+=1
-                queue.append("R")
-            elif queue[l] == "D" and haveR:
-                poppedD+=1
-                haveR-=1
-            elif queue[l] == "D" and not haveR:
-                haveD+=1
-                queue.append("D")
-            l+=1
-        return "Radiant" if poppedD==ndire else "Dire"
+        Rqueue = deque()
+        Dqueue = deque()
+        lastidx = len(senate)
+        for i in range(lastidx):
+            if senate[i] == "R":
+                Rqueue.append(i)
+            else:
+                Dqueue.append(i)
+        while Rqueue and Dqueue:
+            if Rqueue[0]<Dqueue[0]:
+                Dqueue.popleft()
+                Rqueue.popleft()
+                Rqueue.append(lastidx)
+                lastidx+=1
+            else:
+                Dqueue.popleft()
+                Rqueue.popleft()
+                Dqueue.append(lastidx)
+                lastidx+=1
+        return "Radiant" if Rqueue else "Dire"
             
