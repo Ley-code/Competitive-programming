61790f9c010d7377d0b9043e0d99a3f4dd516c33
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-04-18 19:28:01 +0300

    Created 'Course Schedule II' by A2SV Hub

---
+++ 18-Apr-2024/Course Schedule II 127052.py
@@ -0,0 +1,31 @@
+# Problem: Course Schedule II - https://leetcode.com/problems/course-schedule-ii/
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        for nex,pre in prerequisites:
+            graph[pre].append(nex)
+        WHITE = 0
+        GRAY = 1
+        BLACK = 2
+        color = [WHITE]*numCourses
+        stack = []
+        flag = False
+        def dfs(node):
+            nonlocal color
+            nonlocal stack
+            if color[node]== GRAY:
+                return False
+            color[node] = GRAY
+            for neigh in graph[node]:
+                if color[neigh]==BLACK:
+                    continue
+                if not dfs(neigh):
+                    return False
+            color[node] = BLACK
+            stack.append(node)
+            return True
+        for i in range(numCourses):
+            if color[i]==WHITE:
+                if not dfs(i): return []
+        return stack[::-1]
