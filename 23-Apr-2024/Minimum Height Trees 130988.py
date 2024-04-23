70e2026b138b7ce5836209f359eab150d072fe32
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-04-23 16:36:35 +0300

    Created 'Minimum Height Trees' by A2SV Hub

---
+++ 23-Apr-2024/Minimum Height Trees 130988.py
@@ -0,0 +1,24 @@
+# Problem: Minimum Height Trees - https://leetcode.com/problems/minimum-height-trees/
+
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        graph  = defaultdict(list)
+        indegree = {i:0 for i in range(n)}
+        for src,dest in edges:
+            graph[src].append(dest)
+            graph[dest].append(src)
+            indegree[src]+=1
+            indegree[dest]+=1
+        q = deque([node for node in indegree if indegree[node]==1])
+        while q:
+            if n<=2:
+                return list(q)
+            for i in range(len(q)):
+                node = q.popleft()
+                n-=1
+                for nei in graph[node]:
+                    indegree[nei]-=1
+                    if indegree[nei]==1:
+                        q.append(nei)
+        return [0]
+            
