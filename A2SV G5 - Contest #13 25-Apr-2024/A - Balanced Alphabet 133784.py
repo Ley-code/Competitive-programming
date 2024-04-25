532e1e2d8d5f451c44c78dd2b2e8b0cbf10a7d99
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-04-25 23:32:55 +0300

    Created 'A - Balanced Alphabet' by A2SV Hub

---
+++ A2SV G5 - Contest #13 25-Apr-2024/A - Balanced Alphabet 133784.py
@@ -0,0 +1,8 @@
+# Problem: A - Balanced Alphabet - https://codeforces.com/gym/519135/problem/A
+
+for i in range(int(input())):
+    n,k = map(int,input().split())
+    ans = ""
+    for i in range(n):
+        ans+= chr((i%k+97))
+    print(ans)
