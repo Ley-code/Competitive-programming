13112b4cfa8ef520cfdbc9009c7d4d90e49bfa15
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-04-25 23:32:49 +0300

    Created 'B - Chat' by A2SV Hub

---
+++ A2SV G5 - Contest #13 25-Apr-2024/B - Chat 133783.py
@@ -0,0 +1,25 @@
+# Problem: B - Chat - https://codeforces.com/gym/519135/problem/B
+
+import heapq
+
+
+n,k,q = map(int,input().split())
+t = list(map(int,input().split()))
+heap = []
+seen = set()
+for i in range(q):
+    ty,idx = map(int,input().split())
+    if ty == 1:
+        if len(heap)<k:
+            heapq.heappush(heap,t[idx-1])
+            seen.add(t[idx-1])
+        else:
+            if t[idx-1]>heap[0]:
+                seen.remove(heap[0])
+                heapq.heapreplace(heap,t[idx-1])
+                seen.add(t[idx-1])
+    else:
+        if t[idx-1] in seen:
+            print("YES")
+        else:
+            print("NO")   
