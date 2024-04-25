abed92f5ad77db2c531caf31b4c784eb94a9582c
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-04-25 23:32:44 +0300

    Created 'C - Poker' by A2SV Hub

---
+++ A2SV G5 - Contest #13 25-Apr-2024/C - Poker 133782.py
@@ -0,0 +1,16 @@
+# Problem: C - Poker - https://codeforces.com/gym/519135/problem/C
+
+import heapq
+
+
+for i in range(int(input())):
+    n = int(input())
+    arr = list(map(int,input().split()))
+    heap = []
+    tot = 0
+    for num in arr:
+        if num!=0:
+            heapq.heappush(heap,-num)
+        elif num==0 and heap:
+            tot+= -heapq.heappop(heap)
+    print(tot)
