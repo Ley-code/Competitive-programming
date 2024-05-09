1f9b7cf2f34613d3dc8bb860a60a3857fd956b89
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-05-09 16:38:44 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 09-May-2024/Fedor and New Game 144186.py
@@ -0,0 +1,12 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n,m,k = map(int,input().split())
+count = 0
+arr = []
+for i in range(m+1):
+    arr.append(int(input()))
+for i in range(m):
+    res = bin(arr[-1]^arr[i]).count("1")
+    if res<=k:
+        count+=1
+print(count)
