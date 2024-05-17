1e2a4ca8f1b94d2e97c97fdf7b091cd90f074e71
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-05-17 16:32:36 +0300

    Created 'Turtle vs. Rabbit Race: Optimal Trainings' by A2SV Hub

---
+++ 17-May-2024/Turtle vs. Rabbit Race: Optimal Trainings 153024.py
@@ -0,0 +1,26 @@
+# Problem: Turtle vs. Rabbit Race: Optimal Trainings - https://codeforces.com/contest/1933/problem/E
+
+
+for t in range(int(input())):
+    n = int(input())
+    nums = list(map(int, input().split()))
+    presum = [0]
+    for i in range(n):
+        presum.append(presum[-1]+nums[i])
+
+    for q in range(int(input())):
+        left, u = map(int, input().split())
+        l = left-1
+        r = len(presum) - 1
+        while left < r:
+            mid = (left+r + 1) // 2
+            if presum[mid] <= u+presum[l]:
+                left = mid 
+            else:
+                r = mid - 1 
+
+        if left + 1 < len(presum) and abs(presum[left] - (u + presum[l])) > abs(presum[left + 1] - (u+presum[l]))-1:
+            left += 1
+        print(left, end=" ")
+    print()
+         
