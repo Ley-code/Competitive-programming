c673bc2f15b9a343f6ee474f00f5806a8b35069e
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-05-24 18:59:20 +0300

    Created 'Replace Words' by A2SV Hub

---
+++ 24-May-2024/Replace Words 158749.py
@@ -0,0 +1,33 @@
+# Problem: Replace Words - https://leetcode.com/problems/replace-words/
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = defaultdict(int)
+        for word in dictionary:
+            curr = trie
+            for c in word:
+                if c not in curr:
+                    curr[c] = defaultdict(int)
+                curr = curr[c]
+            curr["is_end"] = True
+        sentence = sentence.split()
+        res  = []
+        for word in sentence:
+            curr = trie
+            ans = ""
+            flag = False
+            for c in word:
+                if c in curr and curr[c]["is_end"]:
+                    res.append(ans+c)
+                    ans+=c
+                    flag = True
+                    break
+                if c not in curr:
+                    ans = ""
+                    flag = True
+                    break
+                curr = curr[c]
+                ans+= c
+            if ans == "" or not flag:
+                res.append(word)
+        return " ".join(res)
