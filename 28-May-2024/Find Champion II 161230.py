5d6255d190d07555fe3e35e52be5f50ce61b5c67
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-05-28 19:48:38 +0300

    Created 'Find Champion II' by A2SV Hub

---
+++ 28-May-2024/Find Champion II 161230.py
@@ -0,0 +1,16 @@
+# Problem: Find Champion II - https://leetcode.com/problems/find-champion-ii/
+
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        indegree = defaultdict(int)
+        for edge in edges:
+            u,v = edge
+            indegree[v] += 1
+        ans = None
+        for i in range(n):
+            if indegree[i] == 0:
+                if ans==None:
+                    ans = i
+                else:
+                    return -1
+        return ans
