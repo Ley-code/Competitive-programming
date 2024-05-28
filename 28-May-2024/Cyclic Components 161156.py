c3a91f1e1e88be2364f9fce86445f3e33ce0d0ce
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-05-28 19:11:59 +0300

    Created 'Cyclic Components' by A2SV Hub

---
+++ 28-May-2024/Cyclic Components 161156.py
@@ -0,0 +1,32 @@
+# Problem: Cyclic Components - https://codeforces.com/problemset/problem/977/E
+
+from collections import defaultdict
+
+
+n,m = map(int,input().split())
+graph = defaultdict(list)
+for _ in range(m):
+    u,v = map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+cycles = 0
+def dfs(node):
+    flag = True
+    stack = [node]
+    while stack:
+        curr = stack.pop()
+        if curr in visited:
+            continue
+        visited.add(curr)
+        if len(graph[curr])!=2:
+            flag = False
+        for neigh in graph[curr]:
+            if neigh not in visited:
+                stack.append(neigh)
+    return flag
+visited =  set()
+for i in range(1,n+1):
+    if i not in visited:
+        if dfs(i):
+            cycles+=1
+print(cycles)
