333a8416b6371081776bc2022abecc4210cf4255
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-08-03 10:35:14 +0300

    Created 'A - Se7en' by A2SV Hub

---
+++ A2SV G5 - Contest #26 03-Aug-2024/A - Se7en 187023.py
@@ -0,0 +1,29 @@
+# Problem: A - Se7en - https://codeforces.com/gym/537362/problem/A
+
+for i in range(int(input())):
+  n = int(input())
+  val = n%7
+  if val==0:
+    print(n)
+  else:
+    num = str(n)
+    one,two = n-val,n+(7-val)
+    val1 = str(one)
+    val2 = str(two)
+    ans = ""
+    count1 = 0
+    for i,c in enumerate(val1):
+      if c!=num[i]:
+        count1+=1
+      if val2[i]!=num[i]:
+        count1-=1
+    if count1>0:
+      print(val2)
+    else:
+      print(val1)  
+
+
+        
+
+
+    
