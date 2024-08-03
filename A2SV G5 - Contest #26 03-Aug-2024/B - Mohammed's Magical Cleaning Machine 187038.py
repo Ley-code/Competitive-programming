73395b36198b35afe5a71c16933828f98cec267f
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-08-03 11:11:30 +0300

    Created 'B - Mohammed's Magical Cleaning Machine' by A2SV Hub

---
+++ A2SV G5 - Contest #26 03-Aug-2024/B - Mohammed's Magical Cleaning Machine 187038.py
@@ -0,0 +1,12 @@
+# Problem: B - Mohammed's Magical Cleaning Machine - https://codeforces.com/gym/537362/problem/B
+
+for i in range(int(input())):
+  n = int(input())
+  arr = list(map(int,input().split()))
+  maxops = 0
+  for dust in arr[:-1]:
+    if dust:
+      maxops+=dust
+    if maxops and dust==0:
+      maxops+=1
+  print(maxops)
