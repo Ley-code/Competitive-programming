3a3005208aba8af288cd89f347e784ce96704b12
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-08-17 01:09:41 +0300

    Created 'A - Collecting Coins' by A2SV Hub

---
+++ A2SV G5 - Contest #28 16-Aug-2024/A - Collecting Coins 197411.py
@@ -0,0 +1,13 @@
+# Problem: A - Collecting Coins - https://codeforces.com/gym/540354/problem/A
+
+for i in range(int(input())):
+  arr = list(map(int,input().split()))
+  maxnum = max(arr[:len(arr)-1])
+  nums = 0
+  for i in range(len(arr)-1):
+    nums+= maxnum-arr[i]
+  if nums>arr[-1] or (arr[-1]-nums)%3:
+    print("NO")
+  else:
+    print("YES")
+  
