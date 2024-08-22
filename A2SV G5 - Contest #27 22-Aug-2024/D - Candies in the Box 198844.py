6bd292ddff3cec4de5480def5640a00959c757cf
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-08-23 00:10:09 +0300

    Created 'D - Candies in the Box' by A2SV Hub

---
+++ A2SV G5 - Contest #27 22-Aug-2024/D - Candies in the Box 198844.py
@@ -0,0 +1,23 @@
+# Problem: D - Candies in the Box - https://codeforces.com/gym/538762/problem/D
+
+n = int(input())
+
+def check(val):
+  eaten = 0
+  candies = n
+  while candies>0:
+    val = min(val,candies)
+    eaten += val
+    candies-=val
+    candies -= candies//10
+  return eaten
+low = 1
+high = n
+while low<=high:
+  mid = low + (high-low)//2
+  if check(mid)*2>= n:
+    high = mid -1
+  else:
+    low = mid + 1
+
+print(low)
