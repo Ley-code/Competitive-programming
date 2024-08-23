b4306211a7c47d46baafd28c194abf7b7a917ab9
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-08-23 22:23:07 +0300

    Created 'Fraction Addition and Subtraction' by A2SV Hub

---
+++ 23-Aug-2024/Fraction Addition and Subtraction 204319.py
@@ -0,0 +1,18 @@
+# Problem: Fraction Addition and Subtraction - https://leetcode.com/problems/fraction-addition-and-subtraction/
+
+from fractions import Fraction
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        s=""
+        num=eval(expression)
+        num1=int(num)
+        print(num1)
+        if num1==num:
+            s=str(num1)
+            s+='/1'
+        else:
+            res=Fraction(num).limit_denominator()
+            print(res)
+            s=str(res)
+        return s
+        
