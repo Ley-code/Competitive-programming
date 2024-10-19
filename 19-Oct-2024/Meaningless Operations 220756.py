ccb31cffe1cbc6ab5f07107663c3b80dc9e1dfe3
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-10-19 18:44:32 +0300

    Created 'Meaningless Operations' by A2SV Hub

---
+++ 19-Oct-2024/Meaningless Operations 220756.py
@@ -0,0 +1,23 @@
+# Problem: Meaningless Operations - https://codeforces.com/problemset/problem/1110/C
+
+from math import sqrt
+
+
+def gcd(num):
+  gd = 1
+  i = 1
+  while i<=sqrt(num):
+    if num%i==0:
+      n = num//i
+      if n!=num:
+        gd = max(gd,n)
+    i+=1
+  return gd
+for i in range(int(input())):
+  a = int(input())
+  binary = bin(a)[2:]
+  length = len(binary)
+  if length==binary.count("1"):
+    print(gcd(a))
+  else:
+    print(pow(2,length)-1)
