8022399a7e40820bcc6eaaab865b9ec84145259d
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2024-10-22 11:40:20 +0300

    Created 'Spiral Matrix IV' by A2SV Hub

---
+++ 22-Oct-2024/Spiral Matrix IV 221325.py
@@ -0,0 +1,47 @@
+# Problem: Spiral Matrix IV - https://leetcode.com/problems/spiral-matrix-iv/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
+        mat = [[-1]*n for i in range(m)]
+        curr = head
+        x = 0
+        y = 0
+        rightB = n-1
+        leftB = 0
+        topB = 0
+        bottomB = m-1
+        while curr:
+            while curr and y<=rightB:
+                mat[x][y] = curr.val
+                curr=curr.next
+                y+=1
+            y-=1
+            x+=1
+            topB+=1
+            while curr and x<=bottomB:
+                mat[x][y] = curr.val
+                curr = curr.next
+                x+=1
+            x-=1
+            y-=1
+            rightB-=1
+            while curr and y>=leftB:
+                mat[x][y] = curr.val
+                curr = curr.next
+                y-=1 
+            y+=1
+            x-=1
+            bottomB-=1
+            while curr and x>=topB:
+                mat[x][y] = curr.val
+                curr = curr.next
+                x-=1
+            x+=1
+            y+=1
+            leftB+=1
+        return mat
