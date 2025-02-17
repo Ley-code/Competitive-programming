4126602d9103e9bd8c09896891636efadbf50a92
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2025-02-17 17:54:46 +0300

    Created 'D - Adjacents, ewww !' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/D - Adjacents, ewww ! 269205.py
@@ -0,0 +1,21 @@
+# Problem: D - Adjacents, ewww ! - https://codeforces.com/gym/588094/problem/D
+
+for _ in range(int(input())):
+  n = int(input())
+  if n==2:
+    print(-1)
+  else:
+    ans = []
+    count = 0
+    temp = []
+    while count<2:
+      
+      for i in range(count+1,(n*n)+1,2):
+        temp.append(i)
+        if len(temp)==n:
+          ans.append(temp)
+          temp = []
+
+      count+=1
+    for row in ans:
+      print(*row)
