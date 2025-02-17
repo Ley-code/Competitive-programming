32cf95825322dbb20602cf81f3a1149245d971f3
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2025-02-17 17:55:07 +0300

    Created 'B - Card Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/B - Card Game 269208.py
@@ -0,0 +1,12 @@
+# Problem: B - Card Game - https://codeforces.com/gym/588094/problem/B
+
+n = int(input())
+a = list(map(int,input().split()))
+maps = [i for i in range(n)]
+maps.sort(key = lambda x:a[x])
+l = 0
+r = n-1
+while l<r:
+  print(maps[l]+1,maps[r]+1)
+  l+=1
+  r-=1
